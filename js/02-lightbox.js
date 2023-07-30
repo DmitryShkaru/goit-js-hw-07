8d9dea444e787c733b43d52d948c76ea82ce25c9
DmitryShkaru <vseeshed@gmail.com>
2023-07-30 17:52:11 +0300

    well done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(
+    (item) =>
+      `<li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        alt="${item.description}"
+      />
+    </a>
+  </li>`
+  )
+  .join("");
+
+galleryEl.insertAdjacentHTML("beforeend", markup);
+
+const Lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+  fadeSpeed: 300,
+});
